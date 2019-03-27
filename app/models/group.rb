2a72d516863b88365cc10fc23c24c2bad27ccf2b
Aki Kahamura <aki0215ka@gmail.com>
2019-03-27 17:09:03 +0900

    model作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,8 @@
 class Group < ApplicationRecord
+    has_many :users, through: :group_users
+    has_many :group_users
+    accepts_nested_attributes_for :group_users
+
+    validates :name, {uniqueness: true}
+    validates :password, {presence: true}
 end
